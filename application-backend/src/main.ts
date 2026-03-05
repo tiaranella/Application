011c94051b1b10d91e428a32d2d52e92f0c50ce8
Tetiana S <tatyanaorlv@gmail.com>
2026-03-05 21:28:43 +0200

    fix: prevent multiple server listen invocations
    Ensure NestApplication.listen is executed only once during the bootstrap phase to resolve ERR_SERVER_ALREADY_LISTEN

--- application-backend/src/main.ts
+++ application-backend/src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8080, '127.0.0.1');
   app.useGlobalFilters(new GlobalExceptionFilter());
-  await app.listen(3000);
 }
 
 bootstrap();
