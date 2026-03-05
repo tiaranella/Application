a32d859a1b3dd37f6aea408e3bc99b7427ae0296
Tetiana S <tatyanaorlv@gmail.com>
2026-03-05 22:49:58 +0200

    docs: configure swagger for api documentation

--- application-backend/src/main.ts
+++ application-backend/src/main.ts
@@ -3,11 +3,23 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/global-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080, '127.0.0.1');
+
   app.useGlobalFilters(new GlobalExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Event Management Application API')
+    .setDescription('This API handles secure JWT authentication, event CRUD operations, and user participation management.')
+    .addBearerAuth()
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document); 
+
+    await app.listen(process.env.PORT ?? 8080, '127.0.0.1');
 }
 
 bootstrap();
